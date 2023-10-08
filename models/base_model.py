7495ecfdfda307a9704a4c7eab30191229d2be3c
connortrue <connor.true.94@gmail.com>
2023-10-08 05:31:52 -0500

    5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Creates the BaseModel for HBnB"""
 import uuid
+from models import storage
 from datetime import datetime
 import time
 
@@ -8,11 +9,13 @@ import time
 class BaseModel:
     """Defines all common attributes/methods for other classes:"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Sets up id, created_at, and updated_at"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if not kwargs:
+            storage.new(self)
 
     def __str__(self):
         """prints class name, self.id, and self.__dict__"""
@@ -24,6 +27,7 @@ class BaseModel:
           with the current datetime"""
         time.sleep(0.1)
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of __dict__"""
