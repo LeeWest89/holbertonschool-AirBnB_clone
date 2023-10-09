d53cbb61d386749b485577e2677be7754c045b4e
connortrue <connor.true.94@gmail.com>
2023-10-09 16:36:27 -0500

    rewrote reload to use eval instead of ifs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -41,27 +41,15 @@ class FileStorage:
         with open(self.__file_path, 'w') as f:
             json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
-    def reload(self):
-        """
-        Deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists ; otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised)
-        """
-
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, 'r') as f:
-                for k, v in json.load(f).items():
-                    if v['__class__'] == 'BaseModel':
-                        self.__objects[k] = BaseModel(**v)
-                    elif v['__class__'] == 'User':
-                        self.__objects[k] = User(**v)
-                    elif v['__class__'] == 'State':
-                        self.__objects[k] = State(**v)
-                    elif v['__class__'] == 'City':
-                        self.__objects[k] = City(**v)
-                    elif v['__class__'] == 'Amenity':
-                        self.__objects[k] = Amenity(**v)
-                    elif v['__class__'] == 'Place':
-                        self.__objects[k] = Place(**v)
-                    elif v['__class__'] == 'Review':
-                        self.__objects[k] = Review(**v)
+def reload(self):
+    """
+    Deserializes the JSON file to __objects (only if the JSON file
+    (__file_path) exists ; otherwise, do nothing. If the file doesn’t
+    exist, no exception should be raised)
+    """
+    if os.path.exists(self.__file_path):
+        with open(self.__file_path, 'r') as f:
+            for k, v in json.load(f).items():
+                cls_name = v['__class__']
+                cls = eval(cls_name)
+                self.__objects[k] = cls(**v)
