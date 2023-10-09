076dea923506ef99dbdbc8f890c1c81c8f8d779c
connortrue <connor.true.94@gmail.com>
2023-10-09 15:57:58 -0500

    8 and 9, adding the classes

--- models/place.py
+++ models/place.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Creates a State class that inherits from BaseModel"""
+from .base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Place class that inherits from BaseModel"""
+
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
--- models/review.py
+++ models/review.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Creates a Review class that inherits from BaseModel"""
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+
+    """Review class that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
