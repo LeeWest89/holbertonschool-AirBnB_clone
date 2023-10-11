84230462de1074e539ed95e8228849eaddbf0fb1
LeeWest89 <6683@holbertonstudents.com>
2023-10-10 20:19:33 -0700

    Update to include attributes in the comments

--- models/review.py
+++ models/review.py
@@ -5,7 +5,13 @@ from .base_model import BaseModel
 
 class Review(BaseModel):
 
-    """Review class that inherits from BaseModel"""
+    """Review class that inherits from BaseModel
+
+    Attributes:
+        place_id (str): place id
+        user_id (str): user id
+        text (str): text of the review
+    """
     place_id = ""
     user_id = ""
     text = ""
