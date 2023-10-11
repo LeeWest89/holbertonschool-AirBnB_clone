e4cadae112a7c72f66953325fe024c94d32e73ec
connortrue <connor.true.94@gmail.com>
2023-10-10 21:22:13 -0500

    added user attribute comments

--- models/user.py
+++ models/user.py
@@ -4,7 +4,13 @@ from .base_model import BaseModel
 
 
 class User(BaseModel):
-    """User class that inherits from BaseModel"""
+    """User class that inherits from BaseModel
+
+        Attributes:
+        email (str): user's email address
+        password (str): email password
+        first_name (str): user's first name
+        last_name (str):: user's last name"""
 
     email = ""
     password = ""
